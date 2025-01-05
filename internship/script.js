6069ad618421fe7c484dacf2996f0f91b2795992
Raudiyah <118118022+Raudiyah@users.noreply.github.com>
2025-01-05 23:35:34 +0500

    Delete internship directory

--- internship/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for contacting me! I’ll get back to you soon.');
-    this.reset();
-});
